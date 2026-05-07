e2feb7f57a5acf48a053859061920d0ac65c7533
xufan <xufan@mylinkpi.com>
2026-05-07 23:23:05 +0800

    refactor(插件安装): 移除已安装插件时的卸载逻辑

--- web/app/components/plugins/install-plugin/install-from-local-package/steps/install.tsx
+++ web/app/components/plugins/install-plugin/install-from-local-package/steps/install.tsx
@@ -8,7 +8,6 @@ import { useEffect, useMemo } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import useCheckInstalled from '@/app/components/plugins/install-plugin/hooks/use-check-installed'
 import { useAppContext } from '@/context/app-context'
-import { uninstallPlugin } from '@/service/plugins'
 import { useInstallPackageFromLocal, usePluginTaskList } from '@/service/use-plugins'
 import { isEqualOrLaterThanVersion } from '@/utils/semver'
 import Card from '../../../card'
@@ -73,9 +72,6 @@ const Installed: FC<Props> = ({
     onStartToInstall?.()
 
     try {
-      if (hasInstalled)
-        await uninstallPlugin(installedInfoPayload.installedId)
-
       const {
         all_installed,
         task_id,
